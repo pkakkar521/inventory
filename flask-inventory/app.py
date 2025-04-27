c88353acac25ac257abe40db1ed18b6f8a1b47e1
pkakkar521 <pkakkar521@gmail.com>
2025-04-27 13:33:36 +0530

    ok

--- flask-inventory/app.py
+++ flask-inventory/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from routes import user1_routes, user2_routes
 from auth import auth_routes
 
-app = Flask(__name__, static_folder='inventory-frontend/build/static', template_folder='inventory-frontend/build')
+app = Flask(__name__, static_folder="../frontend/build/static", template_folder="../frontend/build")
 
 CORS(app)
 
@@ -13,17 +13,14 @@ app.register_blueprint(auth_routes)
 app.register_blueprint(user1_routes, url_prefix="/user1")
 app.register_blueprint(user2_routes, url_prefix="/user2")
 
-# Serve the index.html file
-@app.route('/')
-def serve():
-    return send_from_directory(app.template_folder, 'index.html')
-
-# Serve static assets (JS, CSS, images)
-@app.route('/static/<path:path>')
-def serve_static(path):
-    return send_from_directory(os.path.join(app.static_folder, path))
+# Serve the React app
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve_react_app(path):
+    return send_from_directory(app.template_folder, "index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
